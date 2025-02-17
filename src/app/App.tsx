c1ab1e3981a51afc52b1ef15c0adf01132b18dc2
Daniel <daniel.saneusebio@fitfai.com>
2025-02-17 16:05:39 +0000

    feat: Remove delete account instructions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,6 @@ import Home from './components/Home/Home';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './c-components/Layout/Layout';
 import { NavBarButtons } from './c-components/Layout/models/layoutProps.models';
-import DeleteAccountInstructions from './components/DeleteAccountInstructions/DeleteAccountInstructions';
 import TermsAndConditions from './components/TermsAndConditions/TermsAndConditions';
 import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy';
 
@@ -18,10 +17,6 @@ function App() {
         <Layout navBarButtons={navBarInfo} staticNavbar={true}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route
-              path="/delete-account-instructions"
-              element={<DeleteAccountInstructions />}
-            />
             <Route
               path="/terms-and-conditions"
               element={<TermsAndConditions />}
